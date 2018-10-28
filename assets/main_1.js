42a60206854e136ddd412e4d973d4ff615cdb16a
TOPzGamming <42190880+TOPzGamming@users.noreply.github.com>
2018-10-28 01:49:00 -0400

    Alterações
    add trailers nos filmes. faltando apenas a funcionalidade de + info e ao assistir sair da lista.

--- assets/main_1.js
+++ assets/main_1.js
@@ -11,7 +11,7 @@ request.onreadystatechange = function() {
         weekFilmes(data.results);
     }
 };
-request.open("GET", urlBase+"trending/movie/week?api_key="+apiKey, true);
+request.open("GET", urlBase+"trending/movie/week?api_key="+apiKey+"&language=pt-BR", true);
 request.send();
 
 //Função após resposta XMLHttp
@@ -23,14 +23,15 @@ function weekFilmes(film) {
     title += '<h1>Filmes Em Alta</h1>';
 
     for(i = 0; i < film.length; i++) {
-    	mostar += '<div class="card" style="width: 15rem;">';
-		mostar += '<img class="img-thumbnail" alt="Imagem não Encontrada!" src="'+urlImg+'w200/'+film[i].poster_path+'">';
+    	mostar += '<div class="card" style="width: 20rem;">';
+		mostar += '<img class="img-thumbnail" alt="Imagem não Encontrada!" src="'+urlImg+'w300/'+film[i].poster_path+'">';
 		mostar += '<div class="card-body">';
-		mostar += '<h5 class="text-muted" id="titulo">'+film[i].title+'</h5>';
-        mostar += '<p class="card-text"><small class="text-muted">Data de Lançamento: '+film[i].overview+'</small></p>';
+		mostar += '<h5>'+film[i].title+'</h5>';
+        mostar += '<p class="card-text"><small class="text-muted">Sinopse: '+film[i].overview+'</small></p>';
 		mostar += '<p class="card-text"><small class="text-muted">Data de Lançamento: '+film[i].release_date+'</small></p>';
 		mostar += '<p class="card-text"><small class="text-muted">Média: '+film[i].vote_average+'</small></p>';
-        mostar += '<input type="button" class="btn btn-dark" value="Add Lista Desejo" onClick="filme('+film[i].id+')">';
+        mostar += '<input id="btnButton" type="button" class="btn btn-dark" value="Lista Desejo" onClick="filme('+film[i].id+')">';
+        mostar += '<input type="button" class="btn btn-dark" value="+Informações" onClick="mais('+film[i].id+')">';
 		mostar += '</div>';
 		mostar += '</div>';
     }
@@ -57,7 +58,7 @@ pPesquisa.onclick = function () {
     };
 
     // Abrindo Requisição.
-    request.open("GET", urlBase+'search/movie?api_key='+apiKey+'&query='+pValue, true);
+    request.open("GET", urlBase+'search/movie?api_key='+apiKey+'&query='+pValue+"&language=pt-BR", true);
 
     // Add a função de Submit no Form.
     document.getElementById('form').addEventListener('submit', pesquisarFilmes);
@@ -77,15 +78,15 @@ pPesquisa.onclick = function () {
         title += '<h1>Resultado da Pesquisa</h1>';
 
         for(i = 0; i < film.length; i++) {
-            mostar += '<div class="card" style="width: 15rem;">';
-            mostar += '<img class="img-thumbnail" alt="Imagem não Encontrada!" src="'+urlImg+'w200/'+film[i].poster_path+'">';
+            mostar += '<div class="card" style="width: 20rem;">';
+            mostar += '<img class="img-thumbnail" alt="Imagem não Encontrada!" src="'+urlImg+'w300/'+film[i].poster_path+'">';
             mostar += '<div class="card-body">';
-            mostar += '<h5 class="text-muted" id="titulo">'+film[i].title+'</h5>';
-            mostar += '<p class="card-text"><small class="text-muted">Titulo Original: '+film[i].original_title+'</small></p>'
-            mostar += '<p class="card-text"><small class="text-muted">Data de Lançamento: '+film[i].overview+'</small></p>';
+            mostar += '<h5>'+film[i].title+'</h5>';
+            mostar += '<p class="card-text"><small class="text-muted">Sinopse: '+film[i].overview+'</small></p>';
             mostar += '<p class="card-text"><small class="text-muted">Data de Lançamento: '+film[i].release_date+'</small></p>';
             mostar += '<p class="card-text"><small class="text-muted">Média: '+film[i].vote_average+'</small></p>';
-            mostar += '<input type="button" class="btn btn-dark" value="Add Lista Desejo" onClick="filme('+film[i].id+')">';
+            mostar += '<input id="btnButton" type="button" class="btn btn-dark" value="Lista Desejo" onClick="filme('+film[i].id+')">';
+            mostar += '<input type="button" class="btn btn-dark" value="+Informações" onClick="mais('+film[i].id+')">';
             mostar += '</div>';
             mostar += '</div>';
         }
@@ -97,9 +98,12 @@ pPesquisa.onclick = function () {
 }
 //___________________________________________Fim Search__________________________________________________________________________________________
 
+// Função para pegar o id do filme
 function filme(id) {
+    //Atribuido o resultado em uma variavel
     idFilme = (id);
 
+    //criando requisição
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -107,9 +111,10 @@ function filme(id) {
             Adicionar(data);
         }
     };
-    request.open("GET", urlBase+"movie/"+idFilme+"?api_key="+apiKey, true);
+    request.open("GET", urlBase+"movie/"+idFilme+"?api_key="+apiKey+"&language=pt-BR", true);
     request.send();
-    
+
+//Função onde pega o que tem no local storage, caso nulo, ele atribui um array.     
 function Adicionar(film) {
     var listDesejo = [];
     listDesejo = localStorage.getItem("Lista Desejo");
@@ -117,18 +122,25 @@ function Adicionar(film) {
     if (listDesejo == null) {
         listDesejo = [];
     }
+    //Criando variavel que formata em json o resultado.
     var filme = JSON.stringify({
         id: film.id,
         title: film.title,
         poster_path: film.poster_path,
-        original_title: film.original_title,
         release_date: film.release_date,
         overview: film.overview,
         vote_average: film.vote_average
     });
+    // Adicionando filme
     listDesejo.push(filme);
+
+    // Salvando filme no Local Storage
     localStorage.setItem("Lista Desejo", JSON.stringify(listDesejo));
     alert("Filme Adicionado a lista de Desejo");
     return true;
   }
+}
+
+function mais(id) {
+    // body...
 }
--- assets/main_2.js
+++ assets/main_2.js
@@ -3,7 +3,6 @@ var urlBase ='https://api.themoviedb.org/3/';
 var urlImg = 'https://image.tmdb.org/t/p/';
 var apiKey = "0ee05a8fe5aefee62bbcafda1e92b900";
 
-
 var lDesejo = localStorage.getItem("Lista Desejo");
 lDesejo =  JSON.parse(lDesejo);
 
@@ -18,15 +17,46 @@ for (i in lDesejo) {
 	var filme = JSON.parse(lDesejo[i])
 
 	mostrar += '<div class="card" style="width: 20rem;">';
-   	mostrar += '<img class="img-thumbnail" alt="Imagem não Encontrada!" src="'+urlImg+'w500/'+filme.poster_path+'">';
+   	mostrar += '<img class="img-thumbnail" alt="Imagem não Encontrada!" src="'+urlImg+'w300/'+filme.poster_path+'">';
     mostrar += '<div class="card-body">';
-    mostrar += '<h5 class="text-muted" >'+filme.title+'</h5>';
-    mostrar += '<p class="card-text"><small class="text-muted">Titulo Original: '+filme.original_title+'</small></p>'
-    mostrar += '<p class="card-text"><small class="text-muted">Data de Lançamento: '+filme.overview+'</small></p>';
+    mostrar += '<h5>'+filme.title+'</h5>';
+    mostrar += '<p class="card-text"><small class="text-muted">Sinopse: '+filme.overview+'</small></p>';
     mostrar += '<p class="card-text"><small class="text-muted">Data de Lançamento: '+filme.release_date+'</small></p>';
     mostrar += '<p class="card-text"><small class="text-muted">Média: '+filme.vote_average+'</small></p>';
-    mostrar += '<input type="button" class="btn btn-dark" value="Assistir" onClick="">';
+    mostrar += '<input id="btnButton2" data-toggle="modal" data-target=".bd-example-modal-lg" type="button" class="btn btn-dark" value="Assistir" onClick="assistir('+filme.id+')">';
+    mostrar += '<input type="button" class="btn btn-dark" value="+Informações" onClick="mais('+filme.id+')">';
     mostrar += '</div>';
     mostrar += '</div>';
 }
     document.getElementById('root').innerHTML = mostrar;
+
+function assistir(id) {
+   idFilme = (id);
+
+    //criando requisição
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            fAssistir(data.results);
+        }
+    };
+    request.open("GET", urlBase+"movie/"+idFilme+"/videos?api_key="+apiKey+"&language=pt-BR", true);
+    request.send();
+
+    function fAssistir(film) {
+        var mFrame ="";
+        var i;
+
+        for(i = 0; i < film.length; i++){
+        mFrame += '<h5> '+film[i].name+'</h5>';
+        mFrame += '<iframe width="760px" height="560px" src="https://www.youtube.com/embed/'+film[i].key+'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+        }
+
+        document.getElementById('mFramer').innerHTML = mFrame;
+    }
+}
+
+function mais(id) {
+    // body...
+}
